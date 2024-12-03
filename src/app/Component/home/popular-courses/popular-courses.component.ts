c016f21e7b91ee66ace4dc9f63ec1d2d7074dd49
knowmiq <knowmiq@gmail.com>
2024-12-03 22:36:42 +0530

    Working on chnages in design 02/12/2024

--- src/app/Component/home/popular-courses/popular-courses.component.ts
+++ src/app/Component/home/popular-courses/popular-courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-popular-courses',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './popular-courses.component.html',
   styleUrl: './popular-courses.component.css'
 })
